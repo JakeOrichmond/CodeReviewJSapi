c2fbf81cfa6ea7a09ecf0ac2970bb568d5cd93ca
Joshua Logan <joshlogan32323@gmail.com>
2016-05-29 18:37:17 -0700

    finally got api working correctly, also issues with forms and the submit button resolved, just used a click

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,1 +1,11 @@
+var getRepos = require("./../js/get-repos.js").getRepos;
 
+$(document).ready(function() {
+
+  $("#searchSubmit").click(function() {
+
+    var user = $("#userSearch").val();
+    getRepos(user);
+
+  });
+});
---
+++ js/get-repos.js
@@ -0,0 +1,14 @@
+var apiKey = require('./../.env').apiKey;
+
+exports.getRepos = function(user){
+  $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    response.forEach(function(repo) {
+      repoInfo = "<h3>" + repo.name + "<br>" + "Description: " + repo.description + "</h3>";
+
+      $("#searchResults").append(repoInfo);
+    });
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
