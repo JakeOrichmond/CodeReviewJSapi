91514247146aa9f76b035de8b9b4d6a681fd1db8
Joshua Logan <joshlogan32323@gmail.com>
2016-05-30 17:39:14 -0700

    finished some styling and adjusted the text of results, also adjusted button to have hover properties

--- js/get-repos.js
+++ js/get-repos.js
@@ -4,12 +4,13 @@ exports.getRepos = function(user){
   $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
     response.forEach(function(repo) {
-      repoInfo = "<h3>" + repo.name + "<br>" + "Description: " + repo.description + "</h3>";
+      repoInfo = "<h3>Repository Name: " + repo.name + "<br>" + "Description: " + repo.description + "</h3>";
 
       $("#searchResults").append(repoInfo);
     });
+    $("#username").append("<h1>User: " + user + "</h1>");
   }).fail(function(error){
     console.log(error.responseJSON.message);
-    $("#searchResults").append("Error: No user with that name found.")
+    $("#searchResults").append("Error: No user with that name found.");
   });
 };
